151e30a7ac2b80d9d9ecc672b3e2f3d685909b6f
abdulrawoofali <rawoofabd41@gmail.com>
2020-11-07 18:32:15 +0530

    My Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer);
+const httpServer = http.createServer(handleServer)
 
 
 function handleServer(req, res) {
+
+    if(req.url === '/welcome'){
+        res.write('Welcome to Dominos!');
+        res.end();
+    }
+    if(req.url === '/contact'){
+        let obj = {
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+            };
+        res.write(JSON.stringify(obj));
+        res.end();
+    }
+    else{
+        res.writeHead(404);
+        res.write("404");
+        res.end(); 
+    }
   
 }
 
