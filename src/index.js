ac764f24996a77a0a76f280eb53ab847cbbfffdc
abdulrawoofali <rawoofabd41@gmail.com>
2020-11-07 19:26:35 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer)
+const httpServer = http.createServer(handleServer).listen(8081);
 
 
 function handleServer(req, res) {
 
     if(req.url === '/welcome'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('Welcome to Dominos!');
         res.end();
     }
@@ -14,12 +15,14 @@ function handleServer(req, res) {
             phone: '18602100000',
             email: 'guestcaredominos@jublfood.com'
             };
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(JSON.stringify(obj));
         res.end();
     }
     else{
-        res.writeHead(404);
-        res.write("404");
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        //res.writeHead(404);
+        res.write("status 404");
         res.end(); 
     }
   
