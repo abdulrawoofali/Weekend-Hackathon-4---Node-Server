27cd382439e8b869a13238ec57a10ae51f5a765e
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-10 19:33:32 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,25 +6,26 @@ const httpServer = http.createServer(handleServer).listen(8081);
 function handleServer(req, res) {
 
     if(req.url === '/welcome'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('Welcome to Dominos!');
         res.end();
+        return;
     }
     if(req.url === '/contact'){
         let obj = {
             phone: '18602100000',
             email: 'guestcaredominos@jublfood.com'
             };
-        res.writeHead(200, {'Content-Type': 'text/html'});
+  
         res.write(JSON.stringify(obj));
         res.end();
+        return;
     }
-    else{
+    
         res.writeHead(404, {'Content-Type': 'text/html'});
         //res.writeHead(404);
         res.write("status 404");
         res.end(); 
-    }
+    return;
   
 }
 
