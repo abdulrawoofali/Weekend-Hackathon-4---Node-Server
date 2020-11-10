8ba7230ededb82ebdbd55a82084d04d5bcf71fe6
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-10 19:47:20 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ const httpServer = http.createServer(handleServer).listen(8081);
 function handleServer(req, res) {
 
     if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.setHeader("content-Type", "text/plain");
     res.write("Welcome to Dominos!");
     res.end();
   } else if (req.url === "/contact") {
+    res.statusCode = 200;
+    res.setHeader("content-Type", "application/json");
     res.write(
       JSON.stringify({
         phone: "18602100000",
@@ -18,7 +22,7 @@ function handleServer(req, res) {
     res.end();
   } else {
     res.statusCode = 404;
-    res.write("page not found");
+    res.write("Bad request");
     res.end();
   }
   
